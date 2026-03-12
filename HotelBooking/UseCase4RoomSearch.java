0567504dd648ce70a6d503a9fcbd020ac4dd7f1c
reddy200654 <reddy200654@gmail.com>
2026-03-12 15:34:38 +0530

    Update UseCase4RoomSearch.java

--- HotelBooking/UseCase4RoomSearch.java
+++ HotelBooking/UseCase4RoomSearch.java
@@ -1,27 +1,7 @@
-/**
- * ================================================================
- * MAIN CLASS - UseCase4RoomSearch
- * ================================================================
- *
- * Use Case 4: Room Search & Availability Check
- *
- * Description:
- * This class demonstrates how guests
- * can view available rooms without
- * modifying inventory data.
- *
- * The system enforces read-only access
- * by design and usage discipline.
- *
- * @version 4.0
- */
+
 public class UseCase4RoomSearch {
 
-    /**
-     * Application entry point.
-     *
-     * @param args Command-line arguments
-     */
+   
     public static void main(String[] args) {
         Room singleRoom = new SingleRoom();
         Room doubleRoom = new DoubleRoom();
